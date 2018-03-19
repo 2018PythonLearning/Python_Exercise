fff5fead0a34d4dba9475aa02a3a1c6036df983f
1740133524 <1740133524@qq.com>
2018-03-19 16:58:38 +0800

    3-19

---
+++ KW/Practices/OJ_Practice_Notes/KW/题目记录与通过代码/PAT_BASIC/1002. 写出这个数 (20).py
@@ -0,0 +1,22 @@
+# 文件名:    1002. 写出这个数 (20)
+# 作者:      1740133524
+# 创建时间:      2018/3/19
+
+n = int(input())
+i = 0
+pinyin = ['ling', 'yi', 'er', 'san', 'si', 'wu', 'liu', 'qi', 'ba', 'jiu', 'shi']
+num = []
+while n != 0:
+    i += n % 10
+    n //= 10
+
+while i != 0:
+    num.append(pinyin[i % 10])
+    i //= 10
+
+num.reverse()
+for n in num:
+    if n == num[-1]:
+        print(n)
+    else:
+        print(n, end=' ')
