a9424ec4ed4aec387714f6869a2062d170ba8190
GreatZP <greatzp@greatzp.cn>
2018-03-20 19:58:17 +0800

    3-20 commit

--- KW/Practices/OJ_Practice_Notes/KW/题目记录与通过代码/PAT_BASIC/1002. 写出这个数 (20).py
+++ KW/Practices/OJ_Practice_Notes/KW/题目记录与通过代码/PAT_BASIC/1002. 写出这个数 (20).py
@@ -2,21 +2,18 @@
 # 作者:      1740133524
 # 创建时间:      2018/3/19
 
-n = int(input())
-i = 0
-pinyin = ['ling', 'yi', 'er', 'san', 'si', 'wu', 'liu', 'qi', 'ba', 'jiu', 'shi']
-num = []
-while n != 0:
-    i += n % 10
-    n //= 10
+n = input()
+pinyin = ['ling', 'yi', 'er', 'san', 'si', 'wu', 'liu', 'qi', 'ba', 'jiu']
+num = 0
+for i in n:
+    num += int(i)
+k = 1
+while k <= num // 10:
+    k *= 10
+while k >= 10:
+    print(pinyin[num // k], end=" ")
+    num %= k
+    k //= 10
+print(pinyin[num])
 
-while i != 0:
-    num.append(pinyin[i % 10])
-    i //= 10
 
-num.reverse()
-for n in num:
-    if n == num[-1]:
-        print(n)
-    else:
-        print(n, end=' ')
