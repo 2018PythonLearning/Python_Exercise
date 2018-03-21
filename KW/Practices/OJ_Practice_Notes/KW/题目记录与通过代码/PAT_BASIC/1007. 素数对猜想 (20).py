cda255e62c666b8b320d4856fe0e655681aaeea7
1740133524 <1740133524@qq.com>
2018-03-21 20:13:49 +0800

    3-21

--- KW/Practices/OJ_Practice_Notes/KW/题目记录与通过代码/PAT_BASIC/1007. 素数对猜想 (20).py
+++ KW/Practices/OJ_Practice_Notes/KW/题目记录与通过代码/PAT_BASIC/1007. 素数对猜想 (20).py
@@ -7,17 +7,14 @@ import math
 n = int(input())
 num = []
 i = 0
-# for n1 in range(1, n + 1):
-#     for n2 in range(2, n1):
-#         if n1 % n2 == 0:
-#             n1 = 0
-#             break
-#     if n1 != 0:
-#         num.append(n1)
-
 for n1 in range(1, n + 1):
-    if math.sqrt(n1) != 0:
+    for n2 in range(2, n1):
+        if n1 % n2 == 0:
+            n1 = 0
+            break
+    if n1 != 0:
         num.append(n1)
+
 
 num.append(0)
 
