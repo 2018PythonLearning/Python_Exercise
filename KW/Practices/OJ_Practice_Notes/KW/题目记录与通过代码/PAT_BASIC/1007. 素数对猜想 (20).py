b6df60b21f4204d5f66781b1ad9147fafc675fd0
1740133524 <1740133524@qq.com>
2018-03-21 21:23:30 +0800

    3-21

--- KW/Practices/OJ_Practice_Notes/KW/题目记录与通过代码/PAT_BASIC/1007. 素数对猜想 (20).py
+++ KW/Practices/OJ_Practice_Notes/KW/题目记录与通过代码/PAT_BASIC/1007. 素数对猜想 (20).py
@@ -2,23 +2,23 @@
 # 作者:      1740133524
 # 创建时间:      2018/3/21
 
+import math
+
 n = int(input())
 num = []
 i = 0
-for n1 in range(1, n + 1):
-    for n2 in range(2, n1):
+for n1 in range(3, n + 1):
+    for n2 in range(2, int(math.sqrt(n1) + 1)):
         if n1 % n2 == 0:
             n1 = 0
             break
     if n1 != 0:
         num.append(n1)
 
-
-num.append(0)
-
 for x, y in enumerate(num):
-    if num[x] != 0:
-        if num[x + 1] - num[x] == 2:
+    if x == 0:
+        continue
+    if num[x] - num[x - 1] == 2:
             i += 1
 
 print(i)
