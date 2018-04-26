45e1d90fb9721ba7bbd1dd0f0e7fcf49c1a338e8
1740133524 <1740133524@qq.com>
2018-04-26 22:28:18 +0800

    4-26

--- KW/Practices/数据结构题集/3.26   未完成.cpp
+++ KW/Practices/数据结构题集/3.26   未完成.cpp
@@ -10,10 +10,19 @@
 	}
 }*/
 
-int sqrt(double A, double p, double e)
-{
-	while (p*p - A >= e)
-	{
+// 3.26递归
+double sqrt (double A, double p, double e) {
+if (p * p - A) < e)
+return p;
+else
+return sqrt(A, 1 / 2.0 * (p + A / p), e);
+}
 
-	}
+// 3.26非递归
+double sqrt (double A, double p, double p) {
+while (p * p - A >= e) {
+// 这个题每个函数里只需要递归调用一次自身，且只有一个参数变化，所以栈都不需要
+p = 1 / 2.0 * (p + A / p);
+}
+return p;
 }
--- KW/Practices/数据结构题集/3.27   未完成.cpp
+++ KW/Practices/数据结构题集/3.27   未完成.cpp
@@ -1,3 +1,4 @@
+//递归
 int Ackerman(int m, int n)
 {
 	if (m == 0)
@@ -12,4 +13,48 @@ int Ackerman(int m, int n)
 	{
 		return Ackerman(m - 1, Ackerman(m, n - 1));
 	}
+}
+//非递归
+int akm_ (int m, int n) {
+	int stack[1000];
+	int top = 0; //栈顶指针
+	// 递归转非递归，其实质就是模拟递归入栈出栈的过程
+	if (m == 0)
+		return n + 1;
+	if (n == 0) {
+		stack[top++] = m - 1;
+		stack[top++] = 1;
+	} else {
+		stack[top++] = m - 1;
+		stack[top++] = m;
+		stack[top++] = n - 1;
+	}
+
+	while (top > 1) {
+		n = stack[--top];
+		m = stack[--top];
+		if (m == 0) {
+			stack[top++] = n + 1;
+		}
+
+		if (m != 0 && n == 0) {
+			stack[top++] = m - 1;
+			stack[top++] = 1;
+		}
+		if (m != 0 && n != 0) {
+			stack[top++] = m - 1;
+			stack[top++] = m;
+			stack[top++] = n - 1;
+		}
+	}
+
+	return stack[0];
+}
+
+
+int main() {
+	for (int i = 3; i > 0; i--)
+		for (int j = 4; j > 0; j--)
+			printf("%d %d\n", akm(i, j), akm_(i, j));
+	return 0;
 }
